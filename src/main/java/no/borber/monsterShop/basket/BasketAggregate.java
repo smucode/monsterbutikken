cb1bfd04a08511ce8951d924d0dcc00621737b9c
Knut André <kfu@f5it.no>
2014-09-09 14:24:47 +0200

    BasketAggregate test

--- src/main/java/no/borber/monsterShop/basket/BasketAggregate.java
+++ src/main/java/no/borber/monsterShop/basket/BasketAggregate.java
@@ -4,14 +4,34 @@ package no.borber.monsterShop.basket;
 import no.borber.monsterShop.Event;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BasketAggregate {
     private int id;
     private List<Event> derivedEvents = new ArrayList<Event>();
+    private List<BasketItem> basketItems = new ArrayList<BasketItem>();
+
 
     public BasketAggregate(int id, List<Event> events){
         this.id = id;
+
+
+        events.stream().map(e -> (BasketEvent)e).forEach(be -> {
+            if (be.getType().equals("itemAdded")) {
+                basketItems.add(be.getItem());
+            }
+            else {
+                boolean found = false;
+                for(BasketItem basketItem : basketItems){
+                    if(basketItem.getName().equals(be.getItem().getName())){
+                        basketItems.remove(basketItem);
+                        break;
+                    }
+                }
+            }
+        });
     }
 
     public void addItem(BasketItem foo) {
@@ -20,7 +40,7 @@ public class BasketAggregate {
     }
 
     public List<BasketItem> getItems() {
-        return null;
+        return basketItems;
     }
 
     public List<Event> getDerivedEvents(){
